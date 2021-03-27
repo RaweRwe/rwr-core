fa63931b864b4aa0681a47d1689b8acc939e08f3
RWE <49598935+RaweRwe@users.noreply.github.com>
2021-03-28 01:37:25 +0300

    Delete rwr_event.lua

--- rwr_event.lua
+++
@@ -1,9 +0,0 @@
-RegisterNetEvent('RWRCore:DeleteVehicle')
-AddEventHandler('RWRCore:DeleteVehicle', function()
-	if IsPedInAnyVehicle(GetPlayerPed(-1)) then
-		RWR.DeleteVehicle(GetVehiclePedIsIn(GetPlayerPed(-1), false))
-	else
-		local vehicle = GetClosestVehicle()
-		RWR.DeleteVehicle(vehicle)
-	end
-end)
