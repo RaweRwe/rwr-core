caa47fe82f2f0b24404711a7138ac3097d4f3947
RWE <49598935+RaweRwe@users.noreply.github.com>
2021-03-28 23:15:30 +0300

    Delete locales directory
    Laot-Core'dan alınan dosyalar silindi.

--- locales/en.lua
+++
@@ -1,4 +0,0 @@
-Locales["en"] = {
-    ["RWR_R_A_DICT"] = "Requesting anim dict: ",
-    ["RWR_R_M_HASH"] = "Requesting model hash: ",
-}
--- locales/tr.lua
+++
@@ -1,4 +0,0 @@
-Locales["tr"] = {
-    ["RWR_R_A_DICT"] = "Anim paketi yukleniyor: ",
-    ["RWR_R_M_HASH"] = "Model hash yukleniyor: ",
-}
