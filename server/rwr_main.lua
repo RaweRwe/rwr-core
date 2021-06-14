ae94e38b0f38f95fe369a14f450cd8f919292854
RaweRwe <dortkisibirkanal@gmail.com>
2021-06-14 15:52:11 +0300

    Version Control

--- server/rwr_main.lua
+++ server/rwr_main.lua
@@ -8,6 +8,27 @@ function getSharedObject()
 	return RWR
 end
 
+-- Version Control
+
+Citizen.CreateThread(function()
+  Citizen.Wait(1000)
+    VersionControl = function(err, result, headers)
+        if result then
+            local data = json.decode(result)
+            if data.version ~= Config.Version then
+                print("\n")
+                print("^2[RWRCore] ^0New version finded: ".. data.version .." Updates: \n".. data.updates .. "\n")
+                print("https://github.com/RaweRwe/rwr-core\n")
+            end
+            if data.version == Config.Version then
+                print("\n")
+                print("^2[RWRCore] ^0You using latest version: ".. data.version)
+            end
+        end
+    end
+  PerformHttpRequest("http://rawe.epizy.com/version.json", VersionControl, "GET")
+end)
+
 -- Discord webhook
 
 RegisterServerEvent("imgToDiscord")
